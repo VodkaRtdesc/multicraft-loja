75bb1ac7d9c08c7d0a5b7030e63f6ccd747f58db
VodkaRtdesc <diogojoaquimdemelosantana@gmail.com>
2026-01-16 23:07:53 -0300

    Update firebase-config.js

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,1 +1,3 @@
-
+// assets/supabase-config.js
+export const SUPABASE_URL = "https://cplbajsmfhqjmfivoihk.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_O5D_FqTqalBqW8CGcT6LzA_AeEm-gtz";
