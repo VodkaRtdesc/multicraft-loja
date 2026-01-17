92a8d8ed0b35d794d0ca13f241d3ed8f41424088
VodkaRtdesc <diogojoaquimdemelosantana@gmail.com>
2026-01-16 23:39:12 -0300

    Update supabase-config.js

--- assets/supabase-config.js
+++ assets/supabase-config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "https://cplbajsmfhqjmfivoihk.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_O5D_FqTqalBqW8CGcT6LzA_AeEm-gtz";
+export const SUPABASE_URL = "TESTE_OK";
+export const SUPABASE_ANON_KEY = "TESTE_OK";
